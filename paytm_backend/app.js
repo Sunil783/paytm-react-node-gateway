6c51d868cbb56f9b3f4d13518fa548e124fadc0a
sunil mohanty <sunilmohanty724@gmail.com>
2021-02-01 17:28:38 +0530

    first commit

---
+++ paytm_backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const paymentRoute = require("./paymentRoute");
+
+const port = 5000;
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api", paymentRoute);
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
